645c72924370b24d403a1e1aa26bf15127fca3c5
Ella Musina <flurdulis@yahoo.com>
2018-08-25 19:54:25 -0700

    Navigational content updated

--- marinecorps/styleguide/js/main.js
+++ marinecorps/styleguide/js/main.js
@@ -1,12 +1,12 @@
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {
-  stickIt();
+  collapseHeader();
 };
 
 var header = document.getElementById("navbar");
 var sticky = document.getElementById("main").offsetTop - 120;
 
-function stickIt() {
+function collapseHeader() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
   } else {
