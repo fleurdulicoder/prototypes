7f1fc5bddefa7aa52a8fbdd115cc33e456f317f6
Ella Musina <emusina@Papillon.local>
2018-10-09 11:07:59 -0700

    Adding more scss for banner for marines template dev

--- protos/marines/template/src/js/index.js
+++ protos/marines/template/src/js/index.js
@@ -1,14 +1,16 @@
-import Announcer from './announcement';
-// import '~bootstrap/scss/bootstrap-grid.scss';
+// import Announcer from './announcement';
+// // import '~bootstrap/scss/bootstrap-grid.scss';
 import '../scss/index.scss';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const announcer = new Announcer(
-    'on-point-message',
-    'on-point-chevron-left',
-    'on-point-chevron-right',
-    'on-point-pager',
-    'on-point-close',
-  );
-  announcer.show();
-}, false);
+//
+// document.addEventListener('DOMContentLoaded', () => {
+//   // commenting out for now
+//   const announcer = new Announcer(
+//     'on-point-message',
+//     'on-point-chevron-left',
+//     'on-point-chevron-right',
+//     'on-point-pager',
+//     'on-point-close',
+//   );
+//   announcer.show();
+// }, false);
