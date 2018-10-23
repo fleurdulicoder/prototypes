25b7f6c1b318e698cab3ef217d86f8a5b5544637
Ella Musina <emusina@Papillon.local>
2018-10-23 15:04:23 -0700

    Adding breadcrumbs code

--- protos/marines/template/src/js/index.js
+++ protos/marines/template/src/js/index.js
@@ -1,14 +1,14 @@
-import Announcer from './announcement';
+// import Announcer from './announcement';
 import '../scss/index.scss';
 
-document.addEventListener('DOMContentLoaded', () => {
-  // commenting out for now
-  const announcer = new Announcer(
-    'on-point-message',
-    'on-point-chevron-left',
-    'on-point-chevron-right',
-    'on-point-pager',
-    'on-point-close',
-  );
-  announcer.show();
-}, false);
+// document.addEventListener('DOMContentLoaded', () => {
+//   // commenting out for now
+//   const announcer = new Announcer(
+//     'on-point-message',
+//     'on-point-chevron-left',
+//     'on-point-chevron-right',
+//     'on-point-pager',
+//     'on-point-close',
+//   );
+//   announcer.show();
+// }, false);
