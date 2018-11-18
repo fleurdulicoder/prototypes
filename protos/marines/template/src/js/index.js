531b7a80fe69e10d79efaae530b597fb679c0fec
Ella Musina <emusina@Papillon.local>
2018-11-18 01:41:20 -0700

    Adding smart menus for the template

--- protos/marines/template/src/js/index.js
+++ protos/marines/template/src/js/index.js
@@ -1,14 +1,24 @@
 // import Announcer from './announcement';
 import '../scss/index.scss';
+import $ from 'jquery';
+import 'smartmenus';
+import 'smartmenus-bootstrap-4';
+import 'smartmenus-keyboard';
 
 document.addEventListener('DOMContentLoaded', () => {
-//   // commenting out for now
-//   const announcer = new Announcer(
-//     'on-point-message',
-//     'on-point-chevron-left',
-//     'on-point-chevron-right',
-//     'on-point-pager',
-//     'on-point-close',
-//   );
-//   announcer.show();
+  // commenting out for now
+  // const announcer = new Announcer(
+  //   'on-point-message',
+  //   'on-point-chevron-left',
+  //   'on-poisnt-chevron-right',
+  //   'on-point-pager',
+  //   'on-point-close',
+  // );
+  // announcer.show();
+  $('#navbar-nav').smartmenus({
+    mainMenuSubOffsetX: -1,
+    subMenusSubOffsetX: 10,
+    subMenusSubOffsetY: 0,
+    subIndicators: false,
+  });
 }, false);
