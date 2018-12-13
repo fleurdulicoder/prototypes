f7523d25819910f6cbee6c74264d7d9e9e5258a8
Ella Musina <emusina@Papillon.local>
2018-12-13 16:01:27 -0700

    Adding new features for on-point

---
+++ protos/marines/onpoint-new/js/main.js
@@ -0,0 +1,74 @@
+// on-point announcer
+var Announcer = function(id, btnLeftId, btnRightId, pagerId, btnCloseId) {
+  var close = btnCloseId ? document.getElementById(btnCloseId) : null;
+  var carousel = new Carousel(id, pagerId, btnLeftId, btnRightId);
+
+  function hide() {
+    document.body.className = document.body.className.replace(' with-on-spot', '');
+  }
+
+  function open() {
+    window.setTimeout(function() {
+      document.body.className += ' with-on-spot';
+    }, 1400);
+  }
+
+  if (close) {
+    close.addEventListener('click', hide, false);
+    close.addEventListener('touch', hide, false);
+  }
+
+  open();
+};
+
+var Carousel = function(stripId, pagerId, leftControlId, rightControlId) {
+  var strip, pager, leftControl, rightControl, current, quantity;
+  strip = stripId ? document.getElementById(stripId) : null;
+  pager = pagerId ? document.getElementById(pagerId) : null;
+  leftControl = leftControlId ? document.getElementById(leftControlId) : null;
+  rightControl = rightControlId ? document.getElementById(rightControlId) : null;
+  if (!strip || !leftControl || !rightControl) return;
+
+  function setup() {
+    current = 0;
+    quantity = strip.children.length
+    strip.style.width = quantity * 100 + '%';
+    pager.innerHTML = (current + 1) + ' of ' + quantity;
+    for (var i = 0; i < quantity; i++) {
+      strip.children[i].style.width = 100 / quantity + '%';
+    }
+  }
+
+  function moveLeft() {
+    if (current + 1 <= 0) {
+      pager.innerHTML = (Math.abs(current + 1) + 1) + ' of ' + quantity;
+      TweenMax.to(strip, 0.5, {
+        x: (++current * 100) + '%',
+        ease: 'cubic-bezier(.67,.54,.43,.78)',
+        onComplete: function(){
+          console.log('moving left >>> carousel');
+        }
+      });
+    }
+  }
+
+  function moveRight() {
+    if (Math.abs(current - 1) < quantity) {
+      pager.innerHTML = (Math.abs(current - 1) + 1) + ' of ' + quantity;
+      TweenMax.to(strip, 0.5, {
+        x: (--current * 100) + '%',
+        ease: 'cubic-bezier(.67,.54,.43,.78)',
+        onComplete: function(){
+          console.log('moving right >>> carousel');
+        }
+      });
+    }
+  }
+
+  leftControl.addEventListener('click', moveLeft, false);
+  rightControl.addEventListener('click', moveRight, false);
+  leftControl.addEventListener('touch', moveLeft, false);
+  rightControl.addEventListener('touch', moveRight, false);
+
+  setup();
+};
