03a0dcb0c8baf437cee8e03a34bd9df2c8ee33e5
Ella Musina <emusina@Papillon.local>
2019-01-16 19:31:05 -0700

    New Slideshow that Works

--- protos/marines/slideshow/js/slideshow.js
+++ protos/marines/slideshow/js/slideshow.js
@@ -1,5 +1,5 @@
-var Slideshow = function(obj) {
-  var element = obj.id ? document.getElementById(obj.id) : null;
+var Slideshow = Slideshow || function(config) {
+  var element = config.id ? document.getElementById(config.id) : null;
   if (!element) return;
   var view = element.querySelector('.view');
   var views = view.querySelectorAll('.item');
@@ -8,7 +8,10 @@ var Slideshow = function(obj) {
   var carousel = element.querySelector('.carousel');
   var thumbs = carousel.querySelectorAll('.item');
   if (!views || !prev || !next || !carousel || !thumbs) return;
+
   var currentView = views[0], currentIndex = 0, currentThumb = thumbs[0];
+  var autoRotate = config.autorotate || false;
+  var stopRotate = config.stoprotate || false;
 
   function slideIn(e) {
     console.log(e, e.target);
@@ -83,6 +86,35 @@ var Slideshow = function(obj) {
     timeline.play();
   }
 
+  function moveLeft() {
+    var prevIndex = currentIndex - 1;
+    if (prevIndex <= 0) {
+      prevIndex = thumbs.length - 1;
+    }
+    var thumb = thumbs[prevIndex];
+    slideInFromLeft(thumb);
+  }
+
+  function moveRight() {
+    var nextIndex = currentIndex + 1;
+    if (nextIndex >= thumbs.length) {
+      nextIndex = 0;
+    }
+    var thumb = thumbs[nextIndex];
+    slideInFromRight(thumb);
+  }
+
+  var timer;
+  function rotate() {
+    timer = window.setInterval(function(){
+      moveRight();
+    }, 5000);
+  }
+
+  function pause() {
+    window.clearInterval(timer);
+  }
+
   for (var i = 0, len = views.length; i < len; i++) {
     thumbs[i].view = views[i];
     thumbs[i].index = i;
@@ -90,4 +122,25 @@ var Slideshow = function(obj) {
     thumbs[i].addEventListener('touch', function(e) { slideIn(e); }, false);
   }
 
+  prev.addEventListener('click', function(e) { moveLeft(); }, false);
+  prev.addEventListener('touch', function(e) { moveLeft(); }, false);
+  next.addEventListener('click', function(e) { moveRight(); }, false);
+  next.addEventListener('touch', function(e) { moveRight(); }, false);
+
+  if (stopRotate) {
+    element.addEventListener('mouseover', function() { pause(); }, false);
+    element.addEventListener('mouseout', function() { rotate(); }, false);
+  }
+
+  if (autoRotate) {
+    window.addEventListener('resize', function() { // debouncer
+      pause();
+      if (timer) clearInterval(timer);
+      timer = window.setTimeout(function() {
+        rotate();
+      }, 12000);
+    }, false);
+
+    rotate();
+  }
 };
